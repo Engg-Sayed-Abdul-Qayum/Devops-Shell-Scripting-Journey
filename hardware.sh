e04361c2dceaaba1cd6f310fde8788a69b63d97e
Engg-Sayed-Abdul-Qayum <engg.syedabdulqayum@gmail.com>
2026-08-19 15:36:29 +0530

    Add Scripting Files from Day-1

---
+++ hardware.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Memory space"
+echo "----------------"
+free -h
+
+
+echo "Host details"
+echo "----------------"
+hostname
+hostname -I
+
+
+echo "disk usage"
+echo "----------------"
+df -h
+
---
+++ task-1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+
+echo "Hello, Im from task-1.sh"
+
+./task-2.sh
