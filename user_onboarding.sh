f3f5309eb8c5685b50e7dda9d95ff2eac8f13b1f
Engg-Sayed-Abdul-Qayum <engg.syedabdulqayum@gmail.com>
2026-08-20 18:58:44 +0530

    Add Scripting File's from Day-4

---
+++ user_onboarding.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "User Onboarding ....."
+read -p "Enter your last name : " name
+read -p "Enter your ID        : " id
+
+details(){
+    echo "Username is --> $name"
+    echo "User ID is --> $id"
+}
+
+workspace(){
+    echo "Currently i am at ......"
+    pwd
+    mkdir $name
+    echo "Folder created successfully !!!"
+}
+
+workspace
+
+createfile(){
+    cd $name
+    touch $id.txt
+}
+
+if [ $id -eq "12345" ]; then
+    createfile
+fi
+
+
+details 
